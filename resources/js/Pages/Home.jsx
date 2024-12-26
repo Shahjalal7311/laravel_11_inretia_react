274793137ab5c84902111a93bdfd0f43fd79a406
Shahjalal <mdjalal2012@gmail.com>
2024-12-26 09:40:09 +0600

    laravel inertia with react

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home(){
+    return (
+        <>
+            <div class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+                <h3 class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Hello user</h3>
+                <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+                    The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+                </p>
+            </div>
+        </>
+    );
+}
